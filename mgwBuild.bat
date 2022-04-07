156a8fad83ffde3c8ae9ccf2d0bd2f94f3c883e6
WmeldrumU <42564258+WmeldrumU@users.noreply.github.com>
2022-04-07 00:52:31 -0600

    cleanup

--- mgwBuild.bat
+++ mgwBuild.bat
@@ -1,4 +1,5 @@
 if not exist build mkdir build
 cd build
 cmake -S ../ -B . -G "MinGW Makefiles"
-mingw32-make.exe
+mingw32-make.exe  &:: mingw32-make.exe Shaders
+:: remember to append && mingw32-make.exe SHADERS once shaders need to be built
